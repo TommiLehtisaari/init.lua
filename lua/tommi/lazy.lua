96266aa37a0375f3ab510f354a93101ee4f9789c
Tommi Lehtisaari <tommi.lehtisaari@compile.fi>
2023-10-26 21:47:55 +0300

    add symbol usage to help removing unused code

--- lua/tommi/lazy.lua
+++ lua/tommi/lazy.lua
@@ -107,6 +107,15 @@ require("lazy").setup({
     },
   },
 
+  -- Plugin to display references, definitions, and implementations of document symbols 
+  {
+    'Wansmer/symbol-usage.nvim',
+    event = 'BufReadPre', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+    config = function()
+      require('symbol-usage').setup()
+    end
+  },
+
   -- Python
   "mfussenegger/nvim-dap-python",
 
